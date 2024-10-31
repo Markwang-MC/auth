eec65c548cb62c84a587d15b3de8e09ac81f184a
jimmy Wang <Jimmy.w.xuanyu@gmail.com>
2024-10-31 09:45:51 -0400

    update

--- pages/api/mongo.js
+++ pages/api/mongo.js
@@ -3,16 +3,14 @@ import client from "@/lib/mongodb";
 export default async function handler(req, res) {
     console.log('start---------')
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         console.log('链接成功')
-        // Send a ping to confirm a successful connection
         let collection = await client.db("weller").collection('customer');
         const data = await collection.find().toArray();
-    
         console.log("this is user table: ",data);
         return data
       } finally {
+        console.log('finally')
         await client.close();
       }
      
